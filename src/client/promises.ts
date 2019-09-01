e648abbd02b7ab0e98aee7eb0720fbfad2866179
Operations Research Engineering Software+ <alex@oresoftware.com>
2019-08-31 19:12:54 -0700

    remove smoke test export

--- src/client/promises.ts
+++ src/client/promises.ts
@@ -1,10 +1,5 @@
 'use strict';
 
-export const r2gSmokeTest = function () {
-  // r2g command line app uses this exported function
-  return true;
-};
-
 import {LMXClient} from "live-mutex";
 
 const c = new LMXClient();
