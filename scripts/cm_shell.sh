cfde786c089d98c9ef42498bde7ddbf8be86bd73
Operations Research Engineering Software+ <alex@oresoftware.com>
2020-03-20 14:10:17 -0700

    squash-me

---
+++ scripts/cm_shell.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+export cm_override_branch='yes'
---
+++ src/dual/repro.ts
@@ -0,0 +1,34 @@
+import {LMXBroker, LMXClient} from 'live-mutex';
+
+const b = new LMXBroker();
+const c = new LMXClient();
+
+b.emitter.on('warning', w => console.error('broker warning:', w));
+c.emitter.on('warning', w => console.error('client warning', w));
+
+// This function will be called by multiple requests
+// I've just simplified to show the issue
+const main = async () => {
+  // Connect to mutex broker
+  await Promise.all([b.ensure(), c.ensure()]);
+
+  // Lock mutex
+  const lock = await c.acquireLock('registered');
+
+  // if (!lock) {   // if the acquireLock doesn't throw, then lock will always exist
+  //   return;
+  // }
+
+  console.log('lock acquired:', lock);
+
+  // Unlock mutex
+  const released = await c.releaseLock('registered', {id: lock.id});
+
+  console.log('lock released:', released);
+
+
+};
+
+main().catch(error => {
+  console.error(error);
+});
